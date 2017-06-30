d6d2518a92bce8d86209d5a43805b81d1d5bf8ef
jpaitken <juan.aitken@fundacion-jala.org>
2017-06-30 19:15:58 -0400

    updated.

--- src/main/java/org/fundacionjala/sfdc/pages/contacts/ContactsDetail.java
+++ src/main/java/org/fundacionjala/sfdc/pages/contacts/ContactsDetail.java
@@ -3,26 +3,39 @@ package org.fundacionjala.sfdc.pages.contacts;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.fundacionjala.sfdc.framework.utils.CommonActions;
 import org.fundacionjala.sfdc.pages.AssertsDetails;
 import org.fundacionjala.sfdc.pages.MainApp;
 import org.fundacionjala.sfdc.pages.base.DetailBase;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
-import org.openqa.selenium.support.FindBy;
-
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.ASSISTANT;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.ASSISTANT_PHONE;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.BIRTH_DATE;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.CONTACT_NAME;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.DEPARTMENT;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.FIRST_NAME_CATEGORY;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.HOME_PHONE;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.LANGUAGES;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.LAST_NAME;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.LEAD_SOURCE;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.LEVEL;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.MAIL;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.MAILING_CITY;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.MAILING_COUNTRY;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.MAILING_STATE;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.MAILING_STREET;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.MAILING_ZIP;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.MOBILE;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.OTHER_CITY;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.OTHER_COUNTRY;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.OTHER_PHONE;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.OTHER_STATE;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.OTHER_STREET;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.OTHER_ZIP;
+import static org.fundacionjala.sfdc.pages.contacts.ContactFields.PHONE;
 import static org.fundacionjala.sfdc.pages.contacts.ContactFields.TITLE;
 
 /**
@@ -31,71 +44,75 @@ import static org.fundacionjala.sfdc.pages.contacts.ContactFields.TITLE;
 public class ContactsDetail extends DetailBase {
 
     public static final int NAME_LABEL_LENGTH = 3;
-    @FindBy(id = "con2_ileinner")
-    @CacheLookup
+    @FindBy(xpath = "//span[contains(text(), 'Name')]/parent::div/following-sibling::div/span/span")
     private WebElement nameLabel;
 
-    @FindBy(id = "con5_ileinner")
-    @CacheLookup
+    @FindBy(xpath = "//span[contains(text(), 'Phone')]/parent::div/following-sibling::div/span/span")
+    private WebElement phoneLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Account Name')]/parent::div/following-sibling::div/span/div/a")
+    private WebElement accountNameLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Title')]/parent::div/following-sibling::div/span/span")
     private WebElement titleLabel;
 
-    @FindBy(id = "con6_ileinner")
-    @CacheLookup
+    @FindBy(xpath = "//span[contains(text(), 'Department')]/parent::div/following-sibling::div/span/span")
     private WebElement departmentLabel;
 
-    @FindBy(id = "con7_ileinner")
-    @CacheLookup
+    @FindBy(xpath = "//span[contains(text(), 'Birthdate')]/parent::div/following-sibling::div/span/span")
     private WebElement birthDateLabel;
 
-    @FindBy(id = "con9_ileinner")
-    @CacheLookup
+    @FindBy(xpath = "//span[contains(text(), 'Reports To')]/parent::div/following-sibling::div/span/div/a")
+    private WebElement reportToLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Lead Source')]/parent::div/following-sibling::div/span/span")
     private WebElement leadSorceLabel;
 
-    @FindBy(id = "con15_ileinner")
-    @CacheLookup
-    private WebElement mailLabel;
+    @FindBy(xpath = "//span[contains(text(), 'Mailing Address')]/parent::div/following-sibling::div/span/a")
+    private WebElement mailingAddressLabel;
 
-    @FindBy(id = "con19_ileinner")
-    @CacheLookup
-    private WebElement mailingStreet;
+    @FindBy(xpath = "//span[contains(text(), 'Language')]/parent::div/following-sibling::div/span/span")
+    private WebElement languagesLabel;
 
-    @FindBy(id = "con18_ileinner")
-    @CacheLookup
-    private WebElement otherStreet;
+    @FindBy(xpath = "//span[contains(text(), 'Home Phone')]/parent::div/following-sibling::div/span/span")
+    private WebElement homePhoneLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Mobile')]/parent::div/following-sibling::div/span/span")
+    private WebElement mobileLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Other Phone')]/parent::div/following-sibling::div/span/span")
+    private WebElement otherPhoneLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Fax')]/parent::div/following-sibling::div/span/span")
+    private WebElement faxLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Email')]/parent::div/following-sibling::div/span/span/a")
+    private WebElement emailLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Assistant')]/parent::div/following-sibling::div/span/span")
+    private WebElement assistandLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Asst. Phone')]/parent::div/following-sibling::div/span/span")
+    private WebElement assistandPhoneLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Other Address')]/parent::div/following-sibling::div/span/a")
+    private WebElement otherAdressLabel;
+
+    @FindBy(xpath = "//span[contains(text(), 'Level')]/parent::div/following-sibling::div/span/span")
+    private WebElement levelAdress;
+
+    @FindBy(xpath = "//span[text()='Details']")
+    private WebElement datails;
+
+    @FindBy(css = "a[title='Show more actions for this record']")
+    private WebElement showOptionsButton;
 
     /**
      * This method gets name label.
      *
      * @return a name label.
      */
-    public String getNameLabel() {
-        return getTopName().length == NAME_LABEL_LENGTH ? getTopName()[1] : getTopName()[0];
-    }
-
-    /**
-     * This method gets name label.
-     *
-     * @return a name label.
-     */
-    public String getFirstNameCategoryLabel() {
-        return getTopName()[0];
-    }
-
-    /**
-     * This method gets name label.
-     *
-     * @return a name label.
-     */
-    public String getLastNameLabel() {
-        return getTopName().length == NAME_LABEL_LENGTH ? getTopName()[2] : getTopName()[1];
-    }
-
-    /**
-     * This method gets name label.
-     *
-     * @return a name label.
-     */
-    public String[] getTopName() {
+    public String[] getCompleteName() {
         return nameLabel.getText().split(" ");
     }
 
@@ -135,13 +152,14 @@ public class ContactsDetail extends DetailBase {
         return leadSorceLabel.getText();
     }
 
+
     /**
      * This method gets other street label.
      *
      * @return a other street label.
      */
-    private String getOtherStreetLabel() {
-        return otherStreet.getText();
+    private String getOtherAddresLabel() {
+        return otherAdressLabel.getText().split(",")[0];
     }
 
     /**
@@ -149,8 +167,11 @@ public class ContactsDetail extends DetailBase {
      *
      * @return a mailing street label.
      */
-    private String getMailingStreetLabel() {
-        return mailingStreet.getText();
+
+
+    private String getMailingAdressLabel() {
+        return mailingAddressLabel.getText().split(",")[0];
+
     }
 
     /**
@@ -159,7 +180,7 @@ public class ContactsDetail extends DetailBase {
      * @return a mail street label.
      */
     private String getMailLabel() {
-        return mailLabel.getText();
+        return emailLabel.getText();
     }
 
     /**
@@ -177,6 +198,13 @@ public class ContactsDetail extends DetailBase {
         }
         return isElementPresent(contactContainer);
 
+    }
+
+    /**
+     * Click.
+     */
+    public void clickOnDetails() {
+        CommonActions.clickElement(datails);
     }
 
     /**
@@ -208,9 +236,7 @@ public class ContactsDetail extends DetailBase {
      */
     @Override
     public MainApp clickDeleteButton() {
-        CommonActions.clickElement(deleteBtn);
-        Alert javascriptAlert = driver.switchTo().alert();
-        javascriptAlert.accept();
+
         return new MainApp();
     }
 
@@ -222,18 +248,231 @@ public class ContactsDetail extends DetailBase {
     public Map<String, AssertsDetails> getStrategyAssertMap() {
         final Map<String, AssertsDetails> strategyMap = new HashMap<>();
 
-        strategyMap.put(CONTACT_NAME.getValue(), this::getNameLabel);
         strategyMap.put(FIRST_NAME_CATEGORY.getValue(), this::getFirstNameCategoryLabel);
+        strategyMap.put(CONTACT_NAME.getValue(), this::getNameLabel);
         strategyMap.put(LAST_NAME.getValue(), this::getLastNameLabel);
         strategyMap.put(TITLE.getValue(), this::getTitleLabel);
         strategyMap.put(DEPARTMENT.getValue(), this::getDepartmentLabel);
         strategyMap.put(BIRTH_DATE.getValue(), this::getBirthDateLabel);
         strategyMap.put(LEAD_SOURCE.getValue(), this::getLeadSourceLabel);
         strategyMap.put(MAIL.getValue(), this::getMailLabel);
-        strategyMap.put(MAILING_STREET.getValue(), this::getMailingStreetLabel);
-        strategyMap.put(OTHER_STREET.getValue(), this::getOtherStreetLabel);
+        strategyMap.put(MAILING_STREET.getValue(), this::getMailingAdressLabel);
+        strategyMap.put(OTHER_STREET.getValue(), this::getOtherAddresLabel);
+        strategyMap.put(PHONE.getValue(), this::getPhoneLabel);
+        strategyMap.put(HOME_PHONE.getValue(), this::getHomePhoneLabel);
+        strategyMap.put(MOBILE.getValue(), this::getMobileLabel);
+        strategyMap.put(OTHER_PHONE.getValue(), this::getOtherPhoneLabel);
+        strategyMap.put(ASSISTANT.getValue(), this::getAssistandLabel);
+        strategyMap.put(ASSISTANT_PHONE.getValue(), this::getAssistandPhoneLabel);
+        strategyMap.put(MAILING_CITY.getValue(), this::getMailingCity);
+        strategyMap.put(MAILING_STATE.getValue(), this::getMailingState);
+        strategyMap.put(OTHER_CITY.getValue(), this::getOtherCity);
+        strategyMap.put(MAILING_ZIP.getValue(), this::getMailingZip);
+        strategyMap.put(MAILING_COUNTRY.getValue(), this::getMailingCountry);
+        strategyMap.put(OTHER_ZIP.getValue(), this::getOtherZip);
+        strategyMap.put(OTHER_STATE.getValue(), this::getOtherState);
+        strategyMap.put(OTHER_COUNTRY.getValue(), this::getOtherCoutry);
+        strategyMap.put(LANGUAGES.getValue(), this::getLanguagesLabel);
+        strategyMap.put(LEVEL.getValue(), this::getLevelAdress);
+
+
         return strategyMap;
     }
 
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    private String getMailingState() {
+        return mailingAddressLabel.getText().split(",")[2].split(" ")[1];
+    }
 
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    private String getLastNameLabel() {
+        return getCompleteName()[getCompleteName().length - 1];
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    private String getMailingZip() {
+        return mailingAddressLabel.getText().split(",")[2].split(" ")[2];
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    private String getMailingCountry() {
+        return mailingAddressLabel.getText().split(",")[2].split(" ")[3];
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    private String getOtherZip() {
+        return otherAdressLabel.getText().split(",")[2].split(" ")[2];
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    private String getOtherState() {
+        return otherAdressLabel.getText().split(",")[2].split(" ")[1];
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    private String getOtherCoutry() {
+        return otherAdressLabel.getText().split(",")[2].split(" ")[3];
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    private String getFirstNameCategoryLabel() {
+        return getCompleteName()[0];
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getMailingCity() {
+        return mailingAddressLabel.getText().split(",")[1].trim();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getOtherCity() {
+        return otherAdressLabel.getText().split(",")[1].trim();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getNameLabel() {
+        return getCompleteName()[1];
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getPhoneLabel() {
+        return phoneLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getAccountNameLabel() {
+        return accountNameLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getReportToLabel() {
+        return reportToLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getLanguagesLabel() {
+        return languagesLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getHomePhoneLabel() {
+        return homePhoneLabel.getText();
+    }
+
+    /**
+     * Click.
+     * @return label text.
+     */
+    public String getMobileLabel() {
+        return mobileLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getOtherPhoneLabel() {
+        return otherPhoneLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getFaxLabel() {
+        return faxLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getAssistandLabel() {
+        return assistandLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getAssistandPhoneLabel() {
+        return assistandPhoneLabel.getText();
+    }
+
+    /**
+     * Click.
+     *
+     * @return label text.
+     */
+    public String getLevelAdress() {
+        return levelAdress.getText();
+    }
 }
