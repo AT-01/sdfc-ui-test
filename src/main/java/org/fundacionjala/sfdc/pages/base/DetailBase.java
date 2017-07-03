5f30906a5d44807b7b2511a5995206929b130e0d
ReinaldoAguilar <reinaldo.aguilar@fundacion-jala.org>
2017-07-03 19:42:34 +0000

    edit and delete

--- src/main/java/org/fundacionjala/sfdc/pages/base/DetailBase.java
+++ src/main/java/org/fundacionjala/sfdc/pages/base/DetailBase.java
@@ -17,7 +17,7 @@ public abstract class DetailBase extends AbstractBasePage {
     @CacheLookup
     protected WebElement deleteBtn;
 
-    @FindBy(css = "[title='Edit']")
+    @FindBy(xpath = "//button[@title='Edit Status']")
     @CacheLookup
     protected WebElement editBtn;
 
