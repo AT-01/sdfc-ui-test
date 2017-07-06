3bc585d79a894cae09815d7fed552706e2fe21a1
Pablo <pablo.ramirez@fundacion-jala.org>
2017-07-06 09:39:28 -0400

    Improve

--- src/main/java/org/fundacionjala/sfdc/pages/LoginPage.java
+++ src/main/java/org/fundacionjala/sfdc/pages/LoginPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
-import org.fundacionjala.sfdc.framework.selenium.browser.DriverManager;
 import org.fundacionjala.sfdc.framework.selenium.CommonActions;
 import org.fundacionjala.sfdc.framework.utils.Environment;
 import org.fundacionjala.sfdc.pages.base.AbstractBasePage;
