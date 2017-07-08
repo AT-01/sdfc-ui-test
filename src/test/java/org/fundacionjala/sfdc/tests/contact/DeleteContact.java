25767f6772dfc9da861b621dd78d05f27be56905
jpaitken <juan.aitken@fundacion-jala.org>
2017-07-08 18:00:50 -0400

    some improvements

---
+++ src/test/java/org/fundacionjala/sfdc/tests/contact/DeleteContact.java
@@ -0,0 +1,52 @@
+package org.fundacionjala.sfdc.tests.contact;
+
+import java.util.Map;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.fundacionjala.sfdc.framework.selenium.Navigator;
+import org.fundacionjala.sfdc.framework.utils.JsonMapper;
+import org.fundacionjala.sfdc.pages.contacts.ContactFields;
+import org.fundacionjala.sfdc.pages.contacts.ContactForm;
+import org.fundacionjala.sfdc.pages.contacts.ContactHome;
+import org.fundacionjala.sfdc.pages.contacts.ContactsDetail;
+import static org.fundacionjala.sfdc.tests.contact.CreateContact.CONTACT_DATA_PATH;
+import static org.testng.Assert.assertFalse;
+
+/**
+ * Created by Administrator on 7/3/2017.
+ */
+public class DeleteContact {
+    private static final String CONTACT_DATA_EDIT_PATH = "contact/EditContactData.json";
+    private static final String COMMA = ", ";
+    private ContactsDetail contactsDetail;
+    private ContactForm contactForm;
+    private Map<String, String> valuesMapJson;
+    private ContactHome contactHome = new ContactHome();
+
+    /**
+     * This method is a preconditions to edit and delete a contact.
+     */
+    @BeforeMethod
+    public void setUp() {
+        valuesMapJson = JsonMapper.getMapJson(CONTACT_DATA_PATH);
+        Navigator.goToContact();
+        contactForm = contactHome.clickNewButton();
+        contactForm.fillTheForm(valuesMapJson);
+        contactsDetail = contactForm.clickSaveButton();
+
+    }
+
+
+    /**
+     * This method is to delete a contact.
+     */
+    @Test
+    public void xDeleteContact() {
+        contactsDetail.clickDeleteButton();
+        assertFalse(contactsDetail.isContactDisplayed(valuesMapJson.get(ContactFields.CONTACT_NAME.getValue())
+                        .concat(COMMA).concat(valuesMapJson.get(ContactFields.CONTACT_NAME.getValue()))),
+                "The contacts shouldn't to be displayed");
+    }
+}
+
